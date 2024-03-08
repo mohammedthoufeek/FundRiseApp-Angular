3269315adaa94db5dc6792c57b84c6fe233978bc
Atharsh G <atharsh.gkrishnan@gmail.com>
2024-03-08 12:41:52 +0530

    added branches for transaction and bank-account

---
+++ src/app/component/bank-account/bank-account.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-account',
+  standalone: true,
+  imports: [],
+  templateUrl: './bank-account.component.html',
+  styleUrl: './bank-account.component.css'
+})
+export class BankAccountComponent {
+
+}
