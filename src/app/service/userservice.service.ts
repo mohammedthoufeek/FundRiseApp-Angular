4e468b69539d4804e9e9ba857399cd210398141d
Annadurai <SANNADU2@ford.com>
2024-03-14 17:13:45 +0530

    worked on structuring personal messages

--- src/app/service/userservice.service.ts
+++ src/app/service/userservice.service.ts
@@ -13,22 +13,33 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
   private isAuthenticated: boolean = false;
   usermodel:Usermodel=new Usermodel();
+  messageuser:Usermodel=new Usermodel();
   signIn(data:Signin):Observable<Usermodel>{
     return this.httpClient.post("http://localhost:8090/signin", data);
 
   }
-
   signOut():Observable<any>{
     return this.httpClient.get("http://localhost:8090/signout");
   }
   signUp(data:Usermodel):Observable<Usermodel>{
     return this.httpClient.post("http://localhost:8090/user", data);
   }
-
   getIsAuthenticated(): boolean {
     return this.isAuthenticated;
   }
   setIsAuthenticated(data:boolean): void  {
      this.isAuthenticated=data;
+  }
+  getUsers():Observable<any>{
+    return this.httpClient.get("http://localhost:8090/profiles/users");
+  }
+  getInvestors():Observable<any>{
+    return this.httpClient.get("http://localhost:8090/profiles/investors");
+  }
+  getCharity():Observable<any>{
+    return this.httpClient.get("http://localhost:8090/profiles/charity");
+  }
+  getprofileById(id?:number):Observable<Usermodel>{
+    return this.httpClient.get("http://localhost:8090/profile/"+ id);
   }
 }
