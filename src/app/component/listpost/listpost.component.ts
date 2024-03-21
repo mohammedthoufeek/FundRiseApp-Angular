2b26c582b8e5059398ea3d2aedf46e284feec9dd
mohammedthoufeek <www.thoufeekabc@gmail.com>
2024-03-21 15:52:06 +0530

    need to resolve or revert

---
+++ src/app/component/listpost/listpost.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { PostModel } from '../../models/post-model';
+import { CommonModule } from '@angular/common';
+import { PostServiceService } from '../../service/post-service.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-listpost',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './listpost.component.html',
+  styleUrl: './listpost.component.css'
+})
+export class ListpostComponent {
+  posts: PostModel[] = []; 
+
+  constructor(private postService: PostServiceService,private router: Router) {};
+  ngOnInit() {
+    this.postService.getAllPosts().subscribe((data: PostModel[]) => {
+      console.log(data)
+      this.posts = data;
+      console.log("Post data "+this.posts);
+
+    });
+
+    
+  }
+  viewPostDetails(id:  number|undefined ) {
+    this.router.navigate(['/post',id]);
+  }
+
+}
+
